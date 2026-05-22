81602053f2e97fd5fbf969d703600f87640eb6cc
kawser0x <kawserswe@gmail.com>
2026-05-22 23:00:40 +0600

    successfully setup the Project

---
+++ src/router/Router.jsx
@@ -0,0 +1,4 @@
+import { createBrowserRouter } from "react-router";
+import HomePage from "../pages/homePage/HomePage";
+
+export const router = createBrowserRouter([{ path: "/", Component: HomePage }]);
